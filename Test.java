dc58a98f987de0f416e44d79921937c820f20c3c
Santiago D'Ippolito <santidipo@hotmail.com.ar>
2022-05-10 21:44:57 -0300

    Primer Commit

---
+++ Test.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author MegaTecnologia
+ */
+public class Test {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+byte entero = 16;
+boolean logico = true;
+float decimal;
+        decimal = 3;
+String caracter = "santiago";
+long numero = 1600;
+double decimaldoble = 5;
+    }
+           
+    
+}
---
+++ TestOperadores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author MegaTecnologia
+ */
+public class TestOperadores {
+    public static void main(String[] args){
+       int num1 = 5;
+       int num2= 2;
+       int suma= num1 + num2;
+       double division = (num1 / num2);
+       boolean bandera = num1 < num2;
+       System.out.println(suma);
+       System.out.println(division);
+       System.out.println(bandera);
+    }
+}
